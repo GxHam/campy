5ba3defa59a4a1cd329f76ae9e9ad41838d178c5
GxHam <gaoxiang90@gmail.com>
2025-02-06 11:39:51 -0500

    Modify display to tile screen w non basler cameras

--- campy/display.py
+++ campy/display.py
@@ -7,48 +7,73 @@ import matplotlib as mpl
 warnings.filterwarnings("ignore")
 mpl.use('Qt5Agg') # ignore qtapp warning...
 import matplotlib.pyplot as plt
+import screeninfo
 
 
-def DrawFigure(num):
-	mpl.rcParams['toolbar'] = 'None' 
+def DrawFigure(num, position, screen_index=1):
+    mpl.rcParams['toolbar'] = 'None'
 
-	figure = plt.figure(num)
-	ax = plt.axes([0,0,1,1], frameon=False)
+    figure = plt.figure(num)
+    ax = plt.axes([0, 0, 1, 1], frameon=False)
 
-	plt.axis('off')
-	plt.autoscale(tight=True)
-	plt.ion()
+    plt.axis('off')
+    plt.autoscale(tight=True)
+    plt.ion()
 
-	imageWindow = ax.imshow(np.zeros((1,1,3), dtype='uint8'), 
-		interpolation='none')
+    imageWindow = ax.imshow(np.zeros((1, 1, 3), dtype='uint8'),
+                            interpolation='none')
 
-	figure.canvas.draw()
-	plt.show(block=False)
+    # Get screen information
+    monitors = screeninfo.get_monitors()
+    # Ensure there is a second screen; use index 0 for primary if not
+    screen = monitors[screen_index] if len(monitors) > screen_index else monitors[0]
+    screen_width, screen_height = screen.width, screen.height
 
-	return figure, imageWindow
+    # Calculate position based on grid
+    grid_size = (2, 2)  # Example: 2x2 grid
+    tile_width = screen_width // grid_size[0]
+    tile_height = screen_height // grid_size[1]
+
+    x_pos = (position % grid_size[0]) * tile_width
+    y_pos = (position // grid_size[0]) * tile_height
+
+    manager = plt.get_current_fig_manager()
+    # Adjust position by the screen offset
+    manager.window.setGeometry(screen.x + x_pos, screen.y + y_pos, tile_width, tile_height)
+
+    figure.canvas.draw()
+    plt.show(block=False)
+
+    return figure, imageWindow
 
 
 def DisplayFrames(cam_params, dispQueue):
-	n_cam = cam_params['n_cam']
-	
-	if sys.platform == "win32" and cam_params['cameraMake'] == 'basler':
-		# Display on Basler cameras uses the Pylon image window handled by cameras/basler.py
-		pass
-	else:
-		figure, imageWindow = DrawFigure(n_cam+1)
-		while(True):
-			try:
-				if dispQueue:
-					img = dispQueue.popleft()
-					try:
-						imageWindow.set_data(img)
-						figure.canvas.draw()
-						figure.canvas.flush_events()
-					except Exception as e:
-						# logging.error('Caught exception at display.py DisplayFrames: {}'.format(e))
-						pass
-				else:
-					time.sleep(0.01)
-			except KeyboardInterrupt:
-				break
-		plt.close(figure)
+    n_cam = cam_params['n_cam']
+
+    if sys.platform == "win32" and cam_params['cameraMake'] == 'basler':
+        # Display on Basler cameras uses the Pylon image window handled by cameras/basler.py
+        pass
+    else:
+        figures = []
+        for i in range(n_cam + 1):
+            figure, imageWindow = DrawFigure(i, i, screen_index=0)  # Specify secondary display
+            figures.append((figure, imageWindow))
+
+        while True:
+            try:
+                if dispQueue:
+                    img = dispQueue.popleft()
+                    try:
+                        for figure, imageWindow in figures:
+                            imageWindow.set_data(img)
+                            figure.canvas.draw()
+                            figure.canvas.flush_events()
+                    except Exception as e:
+                        # logging.error('Caught exception at display.py DisplayFrames: {}'.format(e))
+                        pass
+                else:
+                    time.sleep(0.01)
+            except KeyboardInterrupt:
+                break
+        for figure, _ in figures:
+            plt.close(figure)
